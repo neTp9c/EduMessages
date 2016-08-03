267eec537265c5fdacc037f7945e8cf87c7b2b6b
Alex Petryakov <alexpetryakov@gmail.com>
2016-08-03 11:03:15 +0300

    Add scaffolds stuffs to display a simple view

---
+++ Messages.Web/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Messages.Web.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Home
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Messages.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Messages.Web.MvcApplication" Language="C#" %>
